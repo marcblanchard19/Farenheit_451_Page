b9c0e0ae74f67cc3c2687a7d42b30452362f393d
mblanchar4 <blanchardolivier91@gmail.com>
2018-06-07 13:53:58 -0400

    Add more to the site. Next, I'll be adding navigation links to the page

---
+++ 451_script.js
@@ -0,0 +1,18 @@
+/*
+Scoll Script
+      function goToByScroll(id){
+                // Reove "link" from the ID
+              id = id.replace("link", "");
+                // Scroll
+              $('html,body').animate({
+                  scrollTop: $("#"+id).offset().top},
+                  'slow');
+          }
+
+          $("#sidebar > ul > li > a").click(function(e) {
+                // Prevent a page reload when a link is pressed
+              e.preventDefault();
+                // Call the scroll function
+              goToByScroll($(this).attr("id"));
+          });
+*/
